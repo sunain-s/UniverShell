49198546c714afde201a1097e10776ed0f8c097f
sunain-s <sas@drsyed.org>
2025-12-27 02:20:36 +0000

    feat(main): main loop entry

--- src/main.c
+++ src/main.c
@@ -1,1 +1,10 @@
-// Main file for UniverShell
+/*------------------------------------------------------------------------------------------
+* File: main.c
+* Description: Entry point for UniverShell application
+* Author: Sunain Syed
+------------------------------------------------------------------------------------------*/
+
+int main(int argc, char **argv) {
+    shell_loop();
+    return EXIT_SUCCESS;
+}
