00d0c24ac2382cc5c92c43d3c69923c6da755c8a
sunain-s <sas@drsyed.org>
2026-01-03 19:16:28 +0000

    feat(builtins): cd and help implementations

--- src/builtins.c
+++ src/builtins.c
@@ -4,6 +4,8 @@
  * Author: Sunain Syed
 -----------------------------------------------------------------------------------------*/
 
+#include <stdio.h>
+
 //-----------------------------------------------------------------------------------------
 // Built-in command declarations
 
@@ -28,4 +30,29 @@ int ush_num_builtins() {
 }
 
 //-----------------------------------------------------------------------------------------
+// Built-in command implementations
 
+int ush_cd(char **args) {
+    if (args[1] == NULL) {
+        fprintf(stderr, "UniverShell: expected argument to \"cd\"\n");
+    } else {
+        if (chdir(args[1]) != 0) {
+            perror("UniverShell");
+        }
+    }
+    return 1;
+}
+
+int ush_help(char **args) {
+    int i;
+    printf("UniverShell: A lightweight cross-platform shell\n");
+    printf("Type program names and arguments, and hit enter.\n");
+    printf("The following are commands are built in:\n");
+
+    for (i = 0; i < ush_num_builtins(); i++) {
+        printf("  %s\n", builtin_str[i]);
+    }
+
+    printf("Use the man command for information on other programs.\n");
+    return 1;
+}
