ffb68027e8a28a105da6a9a7b0bd6a0d59e03ccf
sunain-s <sas@drsyed.org>
2026-01-03 16:53:27 +0000

    feat(shell): launch() to fork processes

--- src/shell.c
+++ src/shell.c
@@ -6,10 +6,33 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 #include "parser.h"
 #include "shell.h"
 
 //-----------------------------------------------------------------------------------------
+
+int launch(char **args) {
+    __pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0) {
+        // Child process
+        if (execvp(args[0], args) == -1) {
+            perror("UniverShell");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        perror("UniverShell");
+    } else {
+        // Parent process
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return 1;
+}
 
 void shell_loop() {
     char *line;
